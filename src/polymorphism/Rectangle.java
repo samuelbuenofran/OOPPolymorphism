5371efd4f7cee7bc31ce86461b0e0c64c6105ef9
Samuel Bueno Francisco <samuel.buenofran@gmail.com>
2024-06-12 21:00:07 -0300

    Updated files

--- src/polymorphism/Rectangle.java
+++ src/polymorphism/Rectangle.java
@@ -1,4 +1,22 @@
 package polymorphism;
 
-public class Rectangle {
+public class Rectangle extends GeometricalShape{
+
+    private double length = 0;
+    private double width = 0;
+    private double area = 0;
+    private double perimeter = 0;
+
+    public Rectangle(double length, double width){
+        this.length = length;
+        this.width = width;
+    }
+
+    public double calculateArea(){
+        return this.length * this.width;
+    }
+
+    public double calculatePerimeter(){
+        return 2 * (this.length + this.width);
+    }
 }
--- src/polymorphism/Triangle.java
+++ src/polymorphism/Triangle.java
@@ -2,10 +2,13 @@ package polymorphism;
 
 public class Triangle extends GeometricalShape {
 
+    // Triangle sides
     private double sideA = 0;
     private double sideB = 0;
     private double sideC = 0;
     private double height = 0;
+    // Area should not be a parameter as it is calculated based on the sides
+    private double area = 0;
 
     @Override
     public double calculatePerimeter(){
@@ -17,8 +20,19 @@ public class Triangle extends GeometricalShape {
         return ((height * sideB) / 2);
     }
 
+    public double heron(){
+        double s = (sideA + sideB + sideC) / 2;
+        return Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
+    }
+
     Triangle(double sideA, double sideB, double sideC, double height){
-        this.sideA = sideA;
+        // Check for triangle inequality violation
+        if (!isValidTriangle(sideA, sideB, sideC)) {
+            throw new IllegalArgumentException("Invalid triangle sides. Please ensure sum of any two sides is greater than the third side.");
+            System.out.println("Error, Triangle object created with invalid sides. Please ensure sum of any two sides is greater than the third side.");
+        }
+
+        this.sideA =  sideA;
         this.sideB = sideB;
         this.sideC = sideC;
         this.height = height;
